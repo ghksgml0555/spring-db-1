8c3d684e0b66e39d8abab21a9abc1a60dc6394f2
dsad2221s <ghksgml0555@naver.com>
2023-03-04 17:32:12 +0900

    h2 db에 연결해서 저장하고 조회
    h2 db에 연결해서 저장하고 조회

---
+++ jdbc/src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
@@ -0,0 +1,105 @@
+package hello.jdbc.repository;
+
+import hello.jdbc.connection.DBConnectionUtil;
+import hello.jdbc.domain.Member;
+import lombok.extern.slf4j.Slf4j;
+
+import java.sql.*;
+import java.util.NoSuchElementException;
+
+/**
+ * JDBC - DriverManager 사용해서 low레벨로 코딩해보기
+ */
+@Slf4j
+public class MemberRepositoryV0 {
+
+    public Member save(Member member) throws SQLException {
+        String sql = "insert into member(member_id, money) values (?, ?)";
+
+        Connection con = null;
+        //PreparedStatement를 가지고 db에 쿼리를 날린다.
+        PreparedStatement pstmt = null;
+
+        try {
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, member.getMemberId());
+            pstmt.setInt(2, member.getMoney());
+            //실행
+            pstmt.executeUpdate();
+            return member;
+        }catch (SQLException e){
+            log.error("db error", e);
+            throw e;
+        }finally {
+            close(con, pstmt, null);
+        }
+    }
+
+    public Member findById(String memberId) throws SQLException {
+        //항상 ?로 파라미터를 바인딩하도록 PreparedStatement를 사용해준다.
+        String sql = "select * from member where member_id = ?";
+
+        //try-catch의 finally에서 con을 호출해야 하기때문에 밖에 선언
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        try {
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, memberId);
+
+            //executeUpdate는 데이터를 변경할 때 사용 select는 executeQuery
+            //executeQuery()는 ResultSet을 반환(셀렉트의 결과를 담고있음)
+            rs = pstmt.executeQuery();
+            //rs.next()를 한번 해줘야 실제 데이터가 있는곳부터 시작한다.
+            //rs.next()를했을때 데이터가 있으면 true없으면 false
+            if(rs.next()){
+                Member member = new Member();
+                member.setMemberId(rs.getString("member_id"));
+                member.setMoney(rs.getInt("money"));
+                return member;
+            }else{
+                throw new NoSuchElementException("member not found memberId=" + memberId);
+            }
+        } catch (SQLException e) {
+            log.error("db error", e);
+            throw e;
+        }finally {
+            close(con,pstmt,rs);
+        }
+    }
+
+    private void close(Connection con, Statement stmt, ResultSet rs){
+        // PreparedStatement는 Statement를 상속받아서 기능이 더 많다.
+        //ResultSet은 뒤에나온다
+        if(rs!=null){
+            try{
+                rs.close();
+            }catch (SQLException e){
+                log.info("error", e);
+            }
+        }
+
+        if(stmt != null){
+            try {
+                stmt.close(); //만약여기서 SQLException이 터져도 캐치로 잡아서 con.close()를 할 수 있다.
+            } catch (SQLException e) {
+                log.error("error", e);
+            }
+        }
+
+        if(con != null){
+            try {
+                con.close();
+            } catch (SQLException e) {
+                log.info("error", e);
+            }
+        }
+    }
+
+    private static Connection getConnection() {
+        return DBConnectionUtil.getConnection();
+    }
+}
