8f36b5934475aaba460846379233b18bc726d17a
dsad2221s <ghksgml0555@naver.com>
2023-03-06 16:04:45 +0900

    @Transactional 사용
    @Transactional 사용

---
+++ jdbc/src/main/java/hello/jdbc/service/MemberServiceV3_3.java
@@ -0,0 +1,43 @@
+package hello.jdbc.service;
+
+import hello.jdbc.domain.Member;
+import hello.jdbc.repository.MemberRepositoryV3;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ * 트랜잭션 - @Transactional AOP
+ */
+@Slf4j
+public class MemberServiceV3_3 {
+
+    private final MemberRepositoryV3 memberRepository;
+
+    public MemberServiceV3_3(MemberRepositoryV3 memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    //이 메서드가 호출될때 트랙잭션을 걸고 시작한다는 의미 >성공하면 커밋 예외터지면 롤백
+    @Transactional
+    public void accountTransfer(String fromId, String toId, int money) throws SQLException {
+                bizLogic(fromId, toId, money);
+    }
+
+    private void bizLogic(String fromId, String toId, int money) throws SQLException {
+        Member fromMember = memberRepository.findById(fromId);
+        Member toMember = memberRepository.findById(toId);
+
+        memberRepository.update(fromId, fromMember.getMoney()- money);
+        validation(toMember);
+        memberRepository.update(toId, toMember.getMoney()+ money);
+    }
+
+    private void validation(Member toMember) {
+        if(toMember.getMemberId().equals("ex")){
+            throw new IllegalStateException("이체중 예외 발생");
+        }
+    }
+}
---
+++ jdbc/src/test/java/hello/jdbc/service/MemberServiceV3_3Test.java
@@ -0,0 +1,118 @@
+package hello.jdbc.service;
+
+import hello.jdbc.domain.Member;
+import hello.jdbc.repository.MemberRepositoryV3;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+import static hello.jdbc.connection.ConnectionConst.*;
+
+/**
+ * 트랜잭션 - @Transactional AOP
+ */
+@Slf4j
+@SpringBootTest //이게있으면 junit이 이걸보고 스프링을 띄워서 스프링빈등록, 의존관계주입도 여기서 가능하게한다.
+class MemberServiceV3_3Test {
+
+    public static final String MEMBER_A = "memberA";
+    public static final String MEMBER_B = "memberB";
+    public static final String MEMBER_EX = "ex";
+
+    //트랜잭션 aop를 적용하려면 스프링컨테이너에 스프링빈을 등록해야할수있다
+    @Autowired
+    private MemberRepositoryV3 memberRepository;
+    @Autowired
+    private MemberServiceV3_3 memberService;
+
+    //트랜잭션 aop를 적용하려면 스프링컨테이너에 스프링빈을 등록해야할수있다
+    @TestConfiguration
+    static class TestConfig{
+        @Bean
+        DataSource dataSource(){
+            return new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+        }
+        @Bean
+        PlatformTransactionManager transactionManager(){
+            return new DataSourceTransactionManager(dataSource());
+        }
+        @Bean
+        MemberRepositoryV3 memberRepositoryV3(){
+            return new MemberRepositoryV3(dataSource());
+        }
+        @Bean
+        MemberServiceV3_3 memberServiceV3_3(){
+            return new MemberServiceV3_3(memberRepositoryV3());
+        }
+    }
+
+
+   /* @BeforeEach
+    void before(){
+        DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+        memberRepository = new MemberRepositoryV3(dataSource);
+        memberService = new MemberServiceV3_3(memberRepository);
+    }*/
+
+    @AfterEach
+    void after() throws SQLException {
+        memberRepository.delete(MEMBER_A);
+        memberRepository.delete(MEMBER_B);
+        memberRepository.delete(MEMBER_EX);
+    }
+
+    @Test
+    @DisplayName("정상 이체")
+    void accountTransfer() throws SQLException{
+        //given
+        Member memberA = new Member(MEMBER_A, 10000);
+        Member memberB = new Member(MEMBER_B, 10000);
+        memberRepository.save(memberA);
+        memberRepository.save(memberB);
+
+        //when
+        memberService.accountTransfer(memberA.getMemberId(), memberB.getMemberId(), 2000);
+
+        //then
+        Member findMemberA = memberRepository.findById(memberA.getMemberId());
+        Member findMemberB = memberRepository.findById(memberB.getMemberId());
+        Assertions.assertThat(findMemberA.getMoney()).isEqualTo(8000);
+        Assertions.assertThat(findMemberB.getMoney()).isEqualTo(12000);
+
+    }
+
+    @Test
+    @DisplayName("이체중 예외 발생")
+    void accountTransferEx() throws SQLException{
+        //given
+        Member memberA = new Member(MEMBER_A, 10000);
+        Member memberEx = new Member(MEMBER_EX, 10000);
+        memberRepository.save(memberA);
+        memberRepository.save(memberEx);
+
+        //when
+        Assertions.assertThatThrownBy(() ->
+                memberService.accountTransfer(memberA.getMemberId(), memberEx.getMemberId(), 2000))
+                .isInstanceOf(IllegalStateException.class);
+
+        //then
+        Member findMemberA = memberRepository.findById(memberA.getMemberId());
+        Member findMemberB = memberRepository.findById(memberEx.getMemberId());
+           //이체중에 예외가 발생해서 rollback되어서 아무 변화가 없다.
+        Assertions.assertThat(findMemberA.getMoney()).isEqualTo(10000);
+        Assertions.assertThat(findMemberB.getMoney()).isEqualTo(10000);
+    }
+}
