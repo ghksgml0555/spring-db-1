6dba7e2901318942c641e892be140c99a574349c
dsad2221s <ghksgml0555@naver.com>
2023-03-07 14:36:51 +0900

    키가 같을때 에러코드를 확인해서 에러를 잡음
    키가 같을때 에러코드를 확인해서 에러를 잡음

---
+++ jdbc/src/main/java/hello/jdbc/repository/ex/MyDuplicateKeyException.java
@@ -0,0 +1,19 @@
+package hello.jdbc.repository.ex;
+
+//런타임익셉션을 상속받은 MyDbException을 받는다.
+public class MyDuplicateKeyException extends MyDbException{
+    public MyDuplicateKeyException() {
+    }
+
+    public MyDuplicateKeyException(String message) {
+        super(message);
+    }
+
+    public MyDuplicateKeyException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public MyDuplicateKeyException(Throwable cause) {
+        super(cause);
+    }
+}
